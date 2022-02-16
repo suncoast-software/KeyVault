d4b2f9a08f02588d583c72018b77b7aeb9fdca49
Async(void) <larose.g@fromtier.com>
2022-02-16 18:33:05 -0500

    ACCOUNTVIEW: worked on accounts view

---
+++ KeyVault/ViewModel/AccountViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyVault.ViewModel
+{
+    internal class AccountViewModel: ViewModelBase
+    {
+
+        private readonly AppDbContextFactory _dbFactory;
+
+        public AccountViewModel()
+        {
+           // _dbFactory = dbFactory;
+            Accounts = new List<Account>();
+            Accounts.Add(new Account { Name = "Google", Url = "www.google.com" });
+            Accounts.Add(new Account { Name = "Inked Apparel", Url = "www.inkedappareltees.com" });
+        }
+
+
+        //we need a List<Account> to hold all the saved accounts from the Database.
+        private List<Account> _accounts;
+        public List<Account> Accounts
+        {
+            get => _accounts;
+            set => OnPropertyChanged(ref _accounts, value);
+        }
+    }
+}
---
+++ KeyVault/ViewModel/SingleAccountRowItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyVault.ViewModel
+{
+    internal class SingleAccountRowItemViewModel: ViewModelBase
+    {
+        private readonly AppDbContextFactory _dbFactory;
+
+        public SingleAccountRowItemViewModel()
+        {
+           
+        }
+
+       
+    }
+}
